2467568ff2a837d9dfab71c2e57a9e55b4d51cda
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2021-11-18 14:12:59 +0100

    Added Ingredient entity + relationship with Recipe

--- src/main/java/it/paofos/spring5recipeapp/domain/Recipe.java
+++ src/main/java/it/paofos/spring5recipeapp/domain/Recipe.java
@@ -1,11 +1,14 @@
 package it.paofos.spring5recipeapp.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -22,6 +25,9 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
 
 	@Lob
 	private Byte[] image;
@@ -107,6 +113,14 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 
 }
