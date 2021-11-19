d2227b9b86dac7043a2871acf604fd4c1dc1d27f
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2021-11-19 17:00:46 +0100

    Added UnitOfMeasure Entity + relationship with Ingredient

--- src/main/java/it/paofos/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/it/paofos/spring5recipeapp/domain/Ingredient.java
@@ -3,10 +3,12 @@ package it.paofos.spring5recipeapp.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -20,6 +22,9 @@ public class Ingredient {
 
 	@ManyToOne
 	private Recipe recipe;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
 
 	public Long getId() {
 		return id;
@@ -51,6 +56,14 @@ public class Ingredient {
 
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUnitOfMeasure() {
+		return unitOfMeasure;
+	}
+
+	public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
+		this.unitOfMeasure = unitOfMeasure;
 	}
 
 }
