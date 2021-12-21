4e47ba0e29fda49745e1dd721a3d0fdd991d9473
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2021-12-21 14:16:54 +0100

    added form to create/edit a recipe

--- src/main/java/it/paofos/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/it/paofos/spring5recipeapp/controllers/RecipeController.java
@@ -2,14 +2,17 @@ package it.paofos.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import it.paofos.spring5recipeapp.commands.RecipeCommand;
 import it.paofos.spring5recipeapp.services.RecipeService;
 
 @Controller
 public class RecipeController {
-	
+
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
@@ -18,10 +21,23 @@ public class RecipeController {
 
 	@RequestMapping("/recipe/show/{id}")
 	public String showById(@PathVariable String id, Model model) {
-		
+
 		model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
-		
+
 		return "recipe/show";
 	}
 
+	@RequestMapping("/recipe/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+
+		return "recipe/recipeForm";
+	}
+
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+		return "redirect:/recipe/show/" + savedCommand.getId();
+	}
 }
