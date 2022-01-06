eb40f835fdc702ba6232cfd0406fc7b762d39a1f
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2022-01-06 18:55:20 +0100

    Persisting images to datbase

--- src/main/java/it/paofos/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/it/paofos/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,18 +1,47 @@
 package it.paofos.spring5recipeapp.services;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import it.paofos.spring5recipeapp.domain.Recipe;
+import it.paofos.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
 
+	private RecipeRepository recipeRepository;
+
+	public ImageServiceImpl(RecipeRepository recipeRepository) {
+		this.recipeRepository = recipeRepository;
+	}
+
 	@Override
 	public void saveImageFile(Long recipeId, MultipartFile file) {
 
-		log.debug("Received a file");
+		try {
+			Recipe recipe = recipeRepository.findById(recipeId).get();
+
+			Byte[] byteObjects = new Byte[file.getBytes().length];
+
+			int i = 0;
+
+			for (byte b : file.getBytes()) {
+				byteObjects[i++] = b;
+			}
+
+			recipe.setImage(byteObjects);
+
+			recipeRepository.save(recipe);
+		} catch (IOException e) {
+			// TODO handle better
+			log.error("Error occurred", e);
+
+			e.printStackTrace();
+		}
 	}
 
 }
