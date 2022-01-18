8ed52a26cb3d4e6fe2743fb943e4ae99f5c1facf
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2022-01-18 14:24:57 +0100

    Spring MVC Controller Advice

--- src/test/java/it/paofos/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/it/paofos/spring5recipeapp/controllers/ImageControllerTest.java
@@ -11,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,7 +43,9 @@ public class ImageControllerTest {
 		MockitoAnnotations.openMocks(this);
 
 		controller = new ImageController(imageService, recipeService);
-		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(controller)//
+				.setControllerAdvice(new ControllerExceptionHandler())//
+				.build();
 	}
 
 	@Test
@@ -100,4 +103,13 @@ public class ImageControllerTest {
 
 		assertEquals(s.getBytes().length, reponseBytes.length);
 	}
+
+	@Test
+	void getImageNumberFormatException() throws Exception {
+
+		mockMvc.perform(get("/recipe/asdf/recipeimage"))//
+				.andExpect(status().isBadRequest())//
+				.andExpect(view().name("400error"));
+	}
+
 }
