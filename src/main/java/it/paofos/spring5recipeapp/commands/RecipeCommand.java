671d5ef9e2c36d9a77ff50dad12130b73b82a3b1
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2022-01-20 09:29:07 +0100

    Data Validation with Spring MVC

--- src/main/java/it/paofos/spring5recipeapp/commands/RecipeCommand.java
+++ src/main/java/it/paofos/spring5recipeapp/commands/RecipeCommand.java
@@ -3,6 +3,13 @@ package it.paofos.spring5recipeapp.commands;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
 import it.paofos.spring5recipeapp.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +20,28 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RecipeCommand {
 	private Long id;
+
+	@NotBlank
+	@Size(min = 3, max = 255)
 	private String description;
+	
+	@Min(1)
+	@Max(999)
 	private Integer prepTime;
+
+	@Min(1)
+	@Max(999)
 	private Integer cookTime;
+
+	@Min(1)
+	@Max(100)
 	private Integer servings;
 	private String source;
+	
+	@URL
 	private String url;
+	
+	@NotBlank
 	private String directions;
 	private Set<IngredientCommand> ingredients = new HashSet<>();
 	private Byte[] image;
--- src/main/java/it/paofos/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/it/paofos/spring5recipeapp/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package it.paofos.spring5recipeapp.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class RecipeController {
 
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeForm";
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
@@ -38,17 +42,25 @@ public class RecipeController {
 	public String newRecipe(Model model) {
 		model.addAttribute("recipe", new RecipeCommand());
 
-		return "recipe/recipeForm";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@GetMapping("recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-		return "recipe/recipeForm";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+			
+			return RECIPE_RECIPEFORM_URL;
+		}
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
